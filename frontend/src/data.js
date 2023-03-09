953a008033b9b3846156c090fcc270ef3264c099
meron nigussie <merontafer@gmail.com>
2023-03-09 07:25:34 +0300

    adding files

---
+++ frontend/src/data.js
@@ -0,0 +1,41 @@
+const data = {
+  products: [
+    {
+      name: 'red lipstick',
+      slug: 'red-lipstick',
+      category: 'lipstick',
+      image: '/images/RED lipstick.jpg',
+      price: 60,
+      countInStock: 10,
+      brand: 'Gucci',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality',
+    },
+    {
+      name: 'purple lipstick',
+      slug: 'purple-lipstick',
+      category: 'lipstick',
+      image: '/images/Purple lipstick.jpeg',
+      price: 60,
+      countInStock: 10,
+      brand: 'Gucci',
+      rating: 4.4,
+      numReviews: 10,
+      description: 'high quality',
+    },
+    {
+      name: 'liquid lipstick',
+      slug: 'liqud-lipstick',
+      category: 'lipstick',
+      image: '/images/Liquid lipstick.jpeg',
+      price: 60,
+      countInStock: 10,
+      brand: '',
+      rating: 4.6,
+      numReviews: 10,
+      description: 'high quality',
+    },
+  ],
+};
+export default data;
